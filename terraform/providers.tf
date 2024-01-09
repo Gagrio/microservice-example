64739515a42d221a0b23610b2bd5b88a0261487a
George <george.agriogiannis@suse.com>
2024-01-09 18:09:06 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,31 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.24.0"
+    }
+    minikube = {
+      source  = "scott-the-programmer/minikube"
+      version = "0.3.7"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+  }
+}
 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+provider "minikube" {
+  kubernetes_version = "v1.28.0"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
