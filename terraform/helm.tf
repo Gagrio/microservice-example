1f4738357f99f92859e10d7a1a35d54dbfdba0a4
George <george.agriogiannis@suse.com>
2024-01-09 19:23:22 +0100

    Update helm.tf

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,1 +1,19 @@
+resource "helm_release" "grafana" {
+  name             = "grafana"
+  namespace        = "monitoring"
+  create_namespace = true
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana"
+  version          = "7.0.21"
+  values           = [file("./helm-values/grafana.yaml")]
+}
 
+resource "helm_release" "prometheus" {
+  name             = "prometheus-community"
+  namespace        = "monitoring"
+  create_namespace = true
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus"
+  version          = "25.8.2"
+  values           = [file("./helm-values/prometheus.yaml")]
+}
